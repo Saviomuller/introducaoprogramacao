b01c6a9d0d2c44e5b8061931d2b9954b1db67697
Saviomuller <saviomuller96@gmail.com>
2025-03-01 22:23:55 -0300

    Aula Operadores
    Operadores básicos

---
+++ aulas/operadores.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+    int main(){
+        
+        /*  
+        Soma (+)
+        Subtração (-)
+        Multiplicação(*)
+        Divisão (/)        
+        */
+
+        int numero1, numero2;
+        int soma, subtracao, multiplicacao, divisao;
+        
+        // Entrada dos dados
+        printf("Entre com o numero 1: \n ");
+        scanf("%d", &numero1);
+        printf("Entre com o numero 2: \n");
+        scanf("%d", &numero2);
+
+        // Operação Soma
+        soma = numero1 + numero2;
+        
+        // Operação Subtração
+        subtracao = numero1 - numero2;
+        
+        // Operação Multiplicação
+        multiplicacao = numero1 * numero2;
+        
+        // Operação Divisão
+        divisao = numero1 / numero2;
+
+        printf("A soma é: %d\n", soma);
+        printf("A subtração é: %d\n", subtracao);
+        printf("A multiplicação é: %d\n", multiplicacao);
+        printf("A divisão é: %d\n", divisao);
+ 
+    }
