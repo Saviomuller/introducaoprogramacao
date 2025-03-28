04f406a27f2234dbf62fc0277069f9feb8474dc6
Saviomuller <saviomuller96@gmail.com>
2025-03-27 23:05:34 -0300

    Update operadorTernario.c
    Adicionado mais exemplos

--- Tema2/operadorTernario.c
+++ Tema2/operadorTernario.c
@@ -6,8 +6,9 @@ int main(){
     int resultadoIdade;
     int temperatura = 31;
     int resultadotemp;
+    int maior, num1 = 40, num2 = 60;
 
-    // Lógica Idade
+    //Lógica Idade
     resultadoIdade = idade >= 18 ? 1 : 0;
     
     if (resultadoIdade ==1){
@@ -19,6 +20,17 @@ int main(){
 
     //Lógica Temperatura
     temperatura > 30 ? printf("Está calor!\n") : printf("Está frio!\n");
+
+    //Número maior
+    num1 > num2 ? (maior = num1) : (maior = num2);
+    printf("O número maior é: %d\n", maior);
+
+    if (num1 > num2) {
+        printf("O numero maior é: %d\n", num1);
+    }
+    else {
+        printf("O numero maior é: %d\n", num2);
+    }
 
     return 0;
 
